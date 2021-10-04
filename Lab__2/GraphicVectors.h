3790b5dfc719793bfa14361ec2e267ccd90fdc38
Canimussurdis <76573311+Canimussurdis@users.noreply.github.com>
2021-10-05 02:01:44 +0300

    Delete Lab__2 directory

--- Lab__2/GraphicVectors.h
+++
@@ -1,9 +0,0 @@
-#ifndef GRAPHICVECTORS_H
-#include <fstream>
-#include <QStandardItemModel>
-#include <handler.h>
-#define GRAPHICVECTORS_H
-
-void setGraphicVectors(vector<Record> records, QVector<double> *x, QVector<double> *y, int col);
-
-#endif // GRAPHICVECTORS_H
